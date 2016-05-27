d2b3f59328fe3cc712b5fbd041fb5648808b8339
Filipe Santos Rocchi <filipe.rocchi@gmail.com>
2016-05-27 20:06:09 -0300

    Added folders for better organization
    main updated with needed addpaths

--- main.m
+++ main.m
@@ -8,13 +8,15 @@
 %
 %
 
-
 %% Initialization
 clear; close all; clc;
 
 %% Adding needed subfolders to path
 addpath('./datasets_functions'); % funcoes de tratamento de dados
-addpath('./RedesNeurais'); % funcao de redes neurais
+addpath('./ANN_files'); % funcao de redes neurais
+addpath('./LR_files'); % funcoes de regressao logistica
+addpath('./SVM_files'); % funcoes de SVM
+addpath('./KNN_files'); % funcoes de KNN
 
 %% Load the train dataset as well as the test dataset 
 fprintf('Carregando os dados do dataset [sem normalizacao] ...\n\n');
